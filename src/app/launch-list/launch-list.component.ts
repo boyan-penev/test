726b5ecbd2a6a2309715c334ad943068058b5728
boyan <none@none.com>
2020-05-22 11:40:50 +0300

    Added a loading animation for the images in the list view

--- src/app/launch-list/launch-list.component.ts
+++ src/app/launch-list/launch-list.component.ts
@@ -2,12 +2,16 @@ import { LaunchFacadeService } from "./../services/launch-facade.service";
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-  selector: "app-launch-list",
-  templateUrl: "./launch-list.component.html",
-  styleUrls: ["./launch-list.component.css"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: "app-launch-list",
+	templateUrl: "./launch-list.component.html",
+	styleUrls: ["./launch-list.component.css"],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LaunchListComponent {
-  constructor(private readonly launchFacade: LaunchFacadeService) {}
-  pastLaunches$ = this.launchFacade.pastLaunchListStoreCache();
+	constructor(private readonly launchFacade: LaunchFacadeService) {}
+	pastLaunches$ = this.launchFacade.pastLaunchListStoreCache();
+
+	public removePlaceholder(event) {
+		event.target.closest('.image-wrapper').classList.remove('loader');
+	}
 }

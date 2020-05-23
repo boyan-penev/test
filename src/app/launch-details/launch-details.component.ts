1c4060054d79a2063d82cde5b8d5919061829f35
boyan <none@none.com>
2020-05-23 16:55:22 +0300

    Details view refactor without jQuery

--- src/app/launch-details/launch-details.component.ts
+++ src/app/launch-details/launch-details.component.ts
@@ -2,8 +2,6 @@ import { Component, ChangeDetectionStrategy, OnInit} from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { map, switchMap } from "rxjs/operators";
 import { LaunchDetailsGQL } from "../services/spacexGraphql.service";
-declare let $: any;
-
 @Component({
   selector: "app-launch-details",
   templateUrl: "./launch-details.component.html",
@@ -11,6 +9,10 @@ declare let $: any;
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LaunchDetailsComponent implements OnInit {
+  thumbnailOffset: number;
+  imagesCount: number;
+  mainImage: string;
+  launchImages: string[]; 
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -18,70 +20,27 @@ export class LaunchDetailsComponent implements OnInit {
     ) {}
 
   ngOnInit() {
-    // jQuery functions to implement the thumbnail slider and the loading of the main image on click
-    // TODO: solve through angular instead
-    $(document).ready(function(){
-
-      // dirty fix to wait for angular to finish loading 
-      setTimeout(function(){ 
-
-        // slides the thumbnails by hiding all at first and then showing those between the offset and offset + 3
-        function slideWrapper(offset) {
-          $thumbnails.hide().filter(index => index >= offset && index < offset + 3).show();
-        }
-
-        // clone the clicked thumbnail and load it in the main image area
-        $('.thumbnail-wrapper').click(function(){
-          $('img.gallery-image').attr('src', $(this).children('img').attr('src'));
-        });
-
-        let thumbnailOffset = 0; // keeps the offset for the thumbnail-slider
-        let $thumbnails = $('.thumbnail-wrapper'); 
-        let imageCount = $thumbnails.length; 
-
-        // all thumbnails are initially hidden, so run the slideWrapper once with zero offset to show the first (max. three) thumbnails
-        slideWrapper(thumbnailOffset);
-
-        // disable the right arrow in the beginning if there are no more than three images
-        if (imageCount <= 3) {
-          $('#thumbnails-slide-right').addClass('disabled');
-        }
-
-        $('#thumbnails-slide-left').addClass('disabled');
-
-        // click on the left arrow
-        $('#thumbnails-slide-left').click(function(){
-
-          if ($(this).hasClass('disabled')) { return false; }
-
-          slideWrapper(--thumbnailOffset);
-
-          // check if the left or right arrow need to be enabled/disabled because of the thumbnail range
-          if (thumbnailOffset == 0) {$(this).addClass('disabled')};
-          if (thumbnailOffset == imageCount - 3) {
-            $('#thumbnails-slide-right').addClass('disabled');
-          } else {
-            $('#thumbnails-slide-right').removeClass('disabled');
-          } 
-        });
-
-        // click on the right arrow
-        $('#thumbnails-slide-right').click(function(){
-          if ($(this).hasClass('disabled')) { return false; }
-
-          slideWrapper(++thumbnailOffset);
-
-          // check if the left or right arrow need to be enabled/disabled because of the thumbnail range
-          $('#thumbnails-slide-left').removeClass('disabled');
-          if (thumbnailOffset == imageCount - 3) {$(this).addClass('disabled');}
-        });
-      }, 1000);
-    });
+    this.launchDetails$.subscribe(launch => this.setLaunchImages(launch.links.flickr_images));
+    this.thumbnailOffset = 0;
   }
 
-  launchDetails$ = this.route.paramMap.pipe(
-    map(params => params.get("id") as string),
-    switchMap(id => this.launchDetailsService.fetch({ id })),
-    map(res => res.data.launch)
-    );
-}
+  setLaunchImages(images): void {
+    this.mainImage = images[0];
+    this.launchImages = images;
+    this.imagesCount = images.length;
+  }
+  
+  slideThumbnails(direction) {
+    if (direction == 'left') { this.thumbnailOffset--; } else if (direction == 'right') { this.thumbnailOffset++; }
+  }
+
+  changeMainImage() {
+      this.mainImage = (event.currentTarget as HTMLLIElement).children[0].getAttribute('src');
+    }
+
+    launchDetails$ = this.route.paramMap.pipe(
+      map(params => params.get("id") as string),
+      switchMap(id => this.launchDetailsService.fetch({ id })),
+      map(res => res.data.launch)
+      );
+  }
